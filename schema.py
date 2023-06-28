7e5b20ac84ede01b557994c89aecc2d361f354a4
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-06-28 21:54:53 +0700

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,30 +1,31 @@
-# build a schema using pydantic
-import models
 from pydantic import BaseModel
 from typing import Optional
 
 
+# Определение модели Book с полями title, rating и author_id
 class Book(BaseModel):
-    title: str
-    rating: int
-    author_id: int
+    title: str  # Название книги
+    rating: int  # Рейтинг книги
+    author_id: int  # Идентификатор автора книги
 
     class Config:
         orm_mode = True
 
 
+# Определение модели BookUpdate для обновления данных книги
 class BookUpdate(BaseModel):
-    title: Optional[str] = None
-    rating: Optional[int] = None
-    author_id: Optional[int] = None
+    title: Optional[str] = None  # Новое название книги (опциональное поле)
+    rating: Optional[int] = None  # Новый рейтинг книги (опциональное поле)
+    author_id: Optional[int] = None  # Новый идентификатор автора книги (опциональное поле)
 
     class Config:
         orm_mode = True
 
 
+# Определение модели Author с полями name и age
 class Author(BaseModel):
-    name: str
-    age: int
+    name: str  # Имя автора
+    age: int  # Возраст автора
 
     class Config:
         orm_mode = True
