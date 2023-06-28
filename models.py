35a99b0e4cebbda7a000a12a01de69dd85473301
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-06-28 21:53:52 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,24 +1,28 @@
+# Импортируем необходимые модули
 import os
-
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker, Session
 from sqlalchemy.sql import func
 from dotenv import load_dotenv
 
+# Создаем базовый класс для объявления моделей
 Base = declarative_base()
 
+# Загружаем переменные окружения из файла .env
 load_dotenv()
 
+# Получаем URL для подключения к базе данных PostgreSQL из переменной окружения
 POSTGRES_URL = os.getenv('DATABASE_URL')
 
-engine = create_engine(
-    POSTGRES_URL, echo=True
-)
+# Создаем движок SQLAlchemy для работы с базой данных
+engine = create_engine(POSTGRES_URL, echo=True)
+
+# Создаем сессию для работы с базой данных
 DBSession = Session(engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
+# Функция для получения экземпляра сессии базы данных
 def get_db():
     db = SessionLocal()
     try:
@@ -26,7 +30,7 @@ def get_db():
     finally:
         db.close()
 
-
+# Определяем модель Book, которая соответствует таблице 'book' в базе данных
 class Book(Base):
     __tablename__ = 'book'
     id = Column(Integer, primary_key=True, index=True)
@@ -38,7 +42,7 @@ class Book(Base):
 
     author = relationship('Author')
 
-
+# Определяем модель Author, которая соответствует таблице 'author' в базе данных
 class Author(Base):
     __tablename__ = 'author'
     id = Column(Integer, primary_key=True)
